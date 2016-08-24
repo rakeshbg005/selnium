0eb8610f88c1dfdd8ba26b418a6d7dd05ae8cf72
rakeshbg005 <rakesh.bg005@gmail.com>
2016-08-24 16:16:52 +0530

    first commit

---
+++ src/test/java/selnium/TestSelenim.java
@@ -0,0 +1,13 @@
+package selnium;
+
+import org.testng.annotations.Test;
+
+public class TestSelenim {
+
+	@Test
+	public void testingmethod(){
+		
+		System.out.println("Testing POM file");
+		
+	}
+}
